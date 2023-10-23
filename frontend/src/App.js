5eb5208ce16f590a4a1bcf249a28942f3c2a14dc
WidnerDerrick-FS <DLWidner@student.fullsail.edu>
2023-10-22 21:15:13 -0400

    The UI is looking good, just have to put in some on click method for the mute button and change the color of the video icon to green later on or change it completely. I have added some glow around the end call button and the avatar as well. I got an AI generated image for the avatar/person that is being used, I plan to blur that image out.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import avatar from './assets/avatar.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideo, faPhone, faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="avatar-container">
+        <img src={avatar} alt="User Avatar" className="avatar" />
+      </div>
+      <span className="call-text">In call with May S...</span>
+      <div className="call-actions">
+        <div className="action-btn">
+          <FontAwesomeIcon icon={faVideo} className='icon'/>
+        </div>
+        <div className="action-btn end-btn">
+          <FontAwesomeIcon icon={faPhone} className='icon'/>
+        </div>
+        <div className="action-btn">
+          <FontAwesomeIcon icon={faMicrophone} className='icon'/>
+        </div>
+      </div>
     </div>
   );
 }
