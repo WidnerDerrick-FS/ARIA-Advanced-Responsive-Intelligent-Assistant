3df85fb7b38d9ca7146c7718ec38d2881d6eea85
WidnerDerrick-FS <DLWidner@student.fullsail.edu>
2023-10-23 05:46:39 -0400

    I have added more design on the frontend of ARIA, making it look like I can call to start the program, mute myself, end the call as well

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,46 +5,60 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo, faPhone, faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  // State variables to track microphone and video states
   const [isMicrophoneMuted, setIsMicrophoneMuted] = useState(false);
-  const [isVideoEnabled, setIsVideoEnabled] = useState(false); // Set to false initially
-
-  // Function to toggle the microphone icon
+  const [isVideoEnabled, setIsVideoEnabled] = useState(false);
+  const [callStatus, setCallStatus] = useState('idle'); // Can be "idle", "calling", or "inCall"
+  
   const toggleMicrophone = () => {
     setIsMicrophoneMuted(!isMicrophoneMuted);
   };
 
-  // Function to toggle the video icon and disable it
   const toggleVideo = () => {
-    // Check if video is enabled, and only toggle if it's enabled
     if (isVideoEnabled) {
       setIsVideoEnabled(false);
     }
   };
+
+  const toggleCall = () => {
+    if (callStatus === 'inCall') {
+      setCallStatus('idle');
+    } else if (callStatus === 'idle') {
+      setCallStatus('calling');
+      setTimeout(() => {
+        setCallStatus('inCall');
+      }, 5000); // 2-second delay for "Calling..."
+    }
+  };
+
+  const getCallText = () => {
+    switch(callStatus) {
+      case 'calling': return 'Calling...';
+      case 'inCall': return 'In call with ARIA';
+      default: return 'Call ARIA';
+    }
+  };
+
+  const getCallButtonClass = () => {
+    return (callStatus === 'calling' || callStatus === 'inCall') ? 'end-btn' : 'call-btn';
+  };
+  
 
   return (
     <div className="app-container">
       <div className="avatar-container">
         <img src={avatar} alt="User Avatar" className={`avatar ${isVideoEnabled ? '' : 'blurred'}`} />
       </div>
-      <span className="call-text">In call with ARIA</span>
+      <span className="call-text">{getCallText()}</span>
       <div className="call-actions">
-        <div className="action-btn">
-          <FontAwesomeIcon
-            icon={faVideo}
-            className={`icon${isVideoEnabled ? '' : ' disabled'}`}
-            onClick={toggleVideo}
-            style={{ cursor: isVideoEnabled ? 'pointer' : 'not-allowed', opacity: isVideoEnabled ? 1 : 0.5 }}
-          />
+        <div className={`action-btn icon${isVideoEnabled ? '' : ' disabled'}`}
+          onClick={toggleVideo}
+          style={{ cursor: isVideoEnabled ? 'pointer' : 'not-allowed', opacity: isVideoEnabled ? 1 : 0.5 }}>
+          <FontAwesomeIcon icon={faVideo} />
         </div>
-        <div className="action-btn end-btn">
+        <div className={`action-btn ${getCallButtonClass()}`} onClick={toggleCall}>
           <FontAwesomeIcon icon={faPhone} className='icon'/>
         </div>
-        <div
-          className={`action-btn microphone ${isMicrophoneMuted ? 'muted' : ''}`}
-          onClick={toggleMicrophone}
-        >
-          {/* Toggle between microphone and microphone-slash icons based on state */}
+        <div className={`action-btn microphone ${isMicrophoneMuted ? 'muted' : ''}`} onClick={toggleMicrophone}>
           <FontAwesomeIcon icon={isMicrophoneMuted ? faMicrophoneSlash : faMicrophone} className='icon'/>
         </div>
       </div>
