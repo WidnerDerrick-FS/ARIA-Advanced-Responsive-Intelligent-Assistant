cf4b10fafc215b092c3dfa7a50fbffce392c0058
WidnerDerrick-FS <DLWidner@student.fullsail.edu>
2023-10-23 06:55:05 -0400

    I changed the audio file to a wav format and everything in the frontend so far is working smoothly and accordingly and has good UX/UI, i havent test it on mobile though.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import avatar from './assets/avatar.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,27 +6,45 @@ import { faVideo, faPhone, faMicrophone, faMicrophoneSlash } from '@fortawesome/
 
 function App() {
   const [isMicrophoneMuted, setIsMicrophoneMuted] = useState(false);
-  const [isVideoEnabled, setIsVideoEnabled] = useState(false);
-  const [callStatus, setCallStatus] = useState('idle'); // Can be "idle", "calling", or "inCall"
+  const [isVideoEnabled, setIsVideoEnabled] = useState(true);
+  const [callStatus, setCallStatus] = useState('idle');
   
+  const audioRef = useRef(null);
+  const ringtone = process.env.PUBLIC_URL + '/aria.wav';
+
+  useEffect(() => {
+    if (callStatus === 'inCall' && !audioRef.current) {
+        audioRef.current = new Audio(ringtone);
+        
+        // Attach an 'ended' event listener to hang up the call when audio completes.
+        audioRef.current.addEventListener('ended', () => {
+            setCallStatus('idle');
+        });
+
+        audioRef.current.play()
+            .catch(error => {
+                console.error("There was an error playing the audio:", error);
+            });
+    } else if (callStatus === 'idle' && audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0; // Reset audio to start
+        audioRef.current = null;
+    }
+}, [callStatus, ringtone]);
+
   const toggleMicrophone = () => {
     setIsMicrophoneMuted(!isMicrophoneMuted);
-  };
-
-  const toggleVideo = () => {
-    if (isVideoEnabled) {
-      setIsVideoEnabled(false);
-    }
   };
 
   const toggleCall = () => {
     if (callStatus === 'inCall') {
       setCallStatus('idle');
+      setIsVideoEnabled(true);
     } else if (callStatus === 'idle') {
       setCallStatus('calling');
       setTimeout(() => {
         setCallStatus('inCall');
-      }, 5000); // 2-second delay for "Calling..."
+      }, 5000);
     }
   };
 
@@ -41,18 +59,16 @@ function App() {
   const getCallButtonClass = () => {
     return (callStatus === 'calling' || callStatus === 'inCall') ? 'end-btn' : 'call-btn';
   };
-  
 
   return (
     <div className="app-container">
       <div className="avatar-container">
-        <img src={avatar} alt="User Avatar" className={`avatar ${isVideoEnabled ? '' : 'blurred'}`} />
+        <img src={avatar} alt="User Avatar" className="avatar blurred" />
       </div>
       <span className="call-text">{getCallText()}</span>
       <div className="call-actions">
-        <div className={`action-btn icon${isVideoEnabled ? '' : ' disabled'}`}
-          onClick={toggleVideo}
-          style={{ cursor: isVideoEnabled ? 'pointer' : 'not-allowed', opacity: isVideoEnabled ? 1 : 0.5 }}>
+        <div className={`action-btn icon disabled`} 
+             style={{ cursor: 'not-allowed', opacity: 0.5 }}>
           <FontAwesomeIcon icon={faVideo} />
         </div>
         <div className={`action-btn ${getCallButtonClass()}`} onClick={toggleCall}>
